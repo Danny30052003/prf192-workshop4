668d137e3dc180bbbe4cc6ce8712eb051917ed02
Danny30052003 <45289938+Danny30052003@users.noreply.github.com>
2022-06-15 22:01:38 +0700

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+void prime(int n) {
+    int i, count;
+    for(i = 1; i <= n/2; i++) {
+       if(n%i == 0) {
+           count++;
+       }
+    }
+    if(count == 2) printf("%d is a Prime\n\n", n);
+    else printf("%d is not a Prime\n\n", n);
+}
+
+void findMaxMin(int n) {
+    int remainder, min, max;
+    remainder = n % 10;
+    n = n / 10;
+    min = max = remainder;
+    while(n>0) {
+        remainder = n % 10;
+        n = n / 10;
+        if(min > remainder) min = remainder;
+        if(max < remainder) max = remainder;
+    }
+    printf("Lowest Number = %d, Highest Number = %d\n\n", min, max);
+}
+
+int main() {
+    int n;
+    printf("----- Menu -----\n");
+    printf("1 - Process primes\n");
+    printf("2 - Print min, max digit in an integer\n");
+    printf("3 - Quit\n");
+    printf("----------------\n");
+    scanf("%d", &n);
+    switch (n) {
+    case 1:
+        printf("\nYou have selected 1!\n");
+        printf("Enter n: ");
+        scanf("%d", &n);
+        prime(n);
+        main();
+        break;
+    case 2:
+        printf("\nYou have selected 2!\n");
+        printf("Enter digit: ");
+        scanf("%d", &n);
+        findMaxMin(n);
+        main();
+        break;
+    case 3:
+        printf("You have quitted the program!\n\n");
+        break;
+    default:
+        printf("\n%d is not an option! Please try again\n\n", n);
+        main();
+        break;
+    }
+}
